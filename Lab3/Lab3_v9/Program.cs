66f56ab900ac4391022129661d87a62aa5c595b6
Kostya9213 <113367241+Kostya9213@users.noreply.github.com>
2022-09-25 22:53:05 +0300

    Add files via upload

---
+++ Lab3/Lab3_v9/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введіть число: ");
+
+            int Number = Convert.ToInt32(Console.ReadLine());
+
+            int Sum = 0;
+
+            if (Number.ToString().Length == 3)
+            {
+                for (int i = Number; i > 0; Sum += i % 10, i /= 10) ;
+
+                if (Sum % 2 == 0)
+                {
+                    Console.WriteLine($"{Sum}: Сума чисел парна");
+                }
+                else
+                {
+                    Console.WriteLine($"{Sum}: Сума чисел не парна");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Помилка, введіть трицифрове число");
+            }
+        }
+    }
+}
