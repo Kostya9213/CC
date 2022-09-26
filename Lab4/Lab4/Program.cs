15d03fb50d910b094381bdacb21858d90c76b2ad
Kostya9213 <113367241+Kostya9213@users.noreply.github.com>
2022-09-26 19:13:12 +0300

    Add files via upload

---
+++ Lab4/Lab4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Lab4_v9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] item = test(new[] { 1, 3, 7, 11, 32 });
+            Console.Write("New array: ");
+            foreach (var i in item)
+            {
+                Console.Write(i.ToString() + " ");
+            }
+        }
+        static int[] test(int[] numbers)
+        {
+            int size = numbers.Length;
+            int[] shiftNums = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                shiftNums[i] = numbers[(i + 1) % size];
+            }
+            return shiftNums;
+        }
+    }
+}
