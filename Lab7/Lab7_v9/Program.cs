0a0957afdc6d4ed90d97666b82d620de8f9dcc14
Kostya9213 <113367241+Kostya9213@users.noreply.github.com>
2023-01-07 17:15:26 +0200

    Add files via upload

---
+++ Lab7/Lab7_v9/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ListExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new List<int>();
+            list.Add(2);
+            list.Add(1);
+
+            foreach (int num in list)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write(num + " ");
+                }
+                Console.WriteLine();
+            }
+
+            list.RemoveAt(0);
+            list.RemoveAt(0);
+
+            Console.WriteLine(string.Join(", ", list));
+        }
+    }
+}
