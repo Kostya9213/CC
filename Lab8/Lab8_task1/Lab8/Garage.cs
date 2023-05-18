39de7d0fb680f9f4fb765f51b6c592f2fc55b8d5
Kostya9213 <113367241+Kostya9213@users.noreply.github.com>
2023-05-18 16:51:16 +0300

    Add files via upload

---
+++ Lab8/Lab8_task1/Lab8/Garage.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Garage
+{
+    private List<Car> cars;
+
+    public Garage()
+    {
+        cars = new List<Car>();
+    }
+
+    public void AddCar(Car car)
+    {
+        cars.Add(car);
+    }
+
+    public void RemoveCar(Car car)
+    {
+        cars.Remove(car);
+    }
+
+    public List<Car> FindCarsByCriteria(Car searchCar)
+    {
+        List<Car> matchingCars = new List<Car>();
+
+        foreach (Car car in cars)
+        {
+            if (MatchCar(car, searchCar))
+            {
+                matchingCars.Add(car);
+            }
+        }
+
+        return matchingCars;
+    }
+
+    private bool MatchCar(Car car, Car searchCar)
+    {
+        return (string.IsNullOrEmpty(searchCar.Name) || car.Name.Equals(searchCar.Name, StringComparison.OrdinalIgnoreCase))
+            && (string.IsNullOrEmpty(searchCar.Color) || car.Color.Equals(searchCar.Color, StringComparison.OrdinalIgnoreCase))
+            && (searchCar.Speed == 0 || car.Speed == searchCar.Speed)
+            && (searchCar.Year == 0 || car.Year == searchCar.Year);
+    }
+}
+
+class Car
+{
+    public string Name { get; set; }
+    public string Color { get; set; }
+    public int Speed { get; set; }
+    public int Year { get; set; }
+
+    public Car(string name, string color, int speed, int year)
+    {
+        Name = name;
+        Color = color;
+        Speed = speed;
+        Year = year;
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Garage sheikhGarage = new Garage();
+
+        Car car1 = new Car("BMW", "Blue", 310, 2019);
+        Car car2 = new Car("Ford", "White", 350, 2015);
+        Car car3 = new Car("Ferrari", "Red", 400, 2022);
+
+        sheikhGarage.AddCar(car1);
+        sheikhGarage.AddCar(car2);
+        sheikhGarage.AddCar(car3);
+
+        Console.WriteLine("All cars in the sheikh's garage:");
+        List<Car> allCars = sheikhGarage.FindCarsByCriteria(new Car(null, null, 0, 0));
+        PrintCars(allCars);
+
+        Console.WriteLine("\nCars with color 'Red':");
+        List<Car> redCars = sheikhGarage.FindCarsByCriteria(new Car(null, "Red", 0, 0));
+        PrintCars(redCars);
+
+        Console.WriteLine("\nCars with speed greater than 350:");
+        List<Car> fastCars = sheikhGarage.FindCarsByCriteria(new Car(null, null, 350, 0));
+        PrintCars(fastCars);
+
+        sheikhGarage.RemoveCar(car2);
+
+        Console.WriteLine("\nAfter removing Ford:");
+        allCars = sheikhGarage.FindCarsByCriteria(new Car(null, null, 0, 0));
+        PrintCars(allCars);
+
+        Console.ReadKey();
+    }
+
+    static void PrintCars(List<Car> cars)
+    {
+        foreach (Car car in cars)
+        {
+            Console.WriteLine($"Name: {car.Name}, Color: {car.Color}, Speed: {car.Speed}, Year: {car.Year}");
+        }
+    }
+}
---
+++ Lab8/Lab8_task2/Lab8_task2/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+class DiskPhone
+{
+    public string PhoneNumber { get; set; }
+    public char[] AvailableSymbols { get; set; }
+
+    public DiskPhone(string phoneNumber, char[] availableSymbols)
+    {
+        PhoneNumber = phoneNumber;
+        AvailableSymbols = availableSymbols;
+    }
+
+    public void MakeCall(string number)
+    {
+        Console.WriteLine($"Dialing {number}...");
+    }
+
+    public void ReceiveCall(string number)
+    {
+        Console.WriteLine($"Incoming call from {number}.");
+    }
+}
+
+class ButtonPhone : DiskPhone
+{
+    public ButtonPhone(string phoneNumber, char[] availableSymbols) : base(phoneNumber, availableSymbols)
+    {
+        AvailableSymbols = new char[availableSymbols.Length + 2];
+        Array.Copy(availableSymbols, AvailableSymbols, availableSymbols.Length);
+        AvailableSymbols[availableSymbols.Length] = '*';
+        AvailableSymbols[availableSymbols.Length + 1] = '#';
+    }
+
+    public new void ReceiveCall(string number)
+    {
+        base.ReceiveCall(number);
+        Console.WriteLine($"Caller ID: {number}");
+    }
+}
+
+class MonochromePhone : ButtonPhone
+{
+    public int Resolution { get; set; }
+    public double ScreenSize { get; set; }
+    public string Color { get; set; }
+
+    public MonochromePhone(string phoneNumber, char[] availableSymbols, int resolution, double screenSize, string color) : base(phoneNumber, availableSymbols)
+    {
+        Resolution = resolution;
+        ScreenSize = screenSize;
+        Color = color;
+    }
+
+    public void SendSMS(string recipient, string message)
+    {
+        Console.WriteLine($"Sending SMS to {recipient}: {message}");
+    }
+
+    public void ReceiveSMS(string sender, string message)
+    {
+        Console.WriteLine($"Received SMS from {sender}: {message}");
+    }
+}
+
+class ColorPhone : MonochromePhone
+{
+    public int NumColors { get; set; }
+    public bool HasSecondSim { get; set; }
+    public string SecondNumber { get; set; }
+
+    public ColorPhone(string phoneNumber, char[] availableSymbols, int resolution, double screenSize, string color, int numColors, bool hasSecondSim, string secondNumber) : base(phoneNumber, availableSymbols, resolution, screenSize, color)
+    {
+        NumColors = numColors;
+        HasSecondSim = hasSecondSim;
+        SecondNumber = secondNumber;
+    }
+
+    public void SendMMS(string recipient, string media)
+    {
+        Console.WriteLine($"Sending MMS to {recipient} with media: {media}");
+    }
+
+    public void ReceiveMMS(string sender, string media)
+    {
+        Console.WriteLine($"Received MMS from {sender} with media: {media}");
+    }
+}
+
+class Smartphone : ColorPhone
+{
+    public bool TouchControl { get; set; }
+    public int MaxTouches { get; set; }
+    public int NumCameras { get; set; }
+
+    public Smartphone(string phoneNumber, char[] availableSymbols, int resolution, double screenSize, string color, int numColors, bool hasSecondSim, string secondNumber, bool touchControl, int maxTouches, int numCameras) : base(phoneNumber, availableSymbols, resolution, screenSize, color, numColors, hasSecondSim, secondNumber)
+    {
+        TouchControl = touchControl;
+        MaxTouches = maxTouches;
+        NumCameras = numCameras;
+    }
+
+    public void TakePhoto()
+    {
+        Console.WriteLine("Taking a photo...");
+    }
+
+    public void RecordVideo()
+    {
+        Console.WriteLine("Recording a video...");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        DiskPhone diskPhone = new DiskPhone("1234567890", new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+        diskPhone.MakeCall("9876543210");
+        diskPhone.ReceiveCall("9876543210");
+
+        ButtonPhone buttonPhone = new ButtonPhone("1234567890", new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' });
+        buttonPhone.MakeCall("9876543210");
+        buttonPhone.ReceiveCall("9876543210");
+
+        MonochromePhone monochromePhone = new MonochromePhone("1234567890", new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }, 480, 4.5, "Black");
+        monochromePhone.SendSMS("9876543210", "Hello!");
+        monochromePhone.ReceiveSMS("9876543210", "Hi!");
+
+        ColorPhone colorPhone = new ColorPhone("1234567890", new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }, 1080, 5.0, "Red", 16, true, "9876543210");
+        colorPhone.SendMMS("9876543210", "image.jpg");
+        colorPhone.ReceiveMMS("9876543210", "video.mp4");
+
+        Smartphone smartphone = new Smartphone("1234567890", new char[] { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' }, 1440, 6.0, "Blue", 32, true, "9876543210", true, 5, 3);
+        smartphone.TakePhoto();
+        smartphone.RecordVideo();
+    }
+}
